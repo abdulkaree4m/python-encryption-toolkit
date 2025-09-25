833a7195a1474988d3210dbf1793b5e85ea64e8e
Abdulkareem <abdulkaree4m@gma1l.com>
2025-09-26 01:16:46 +0300

    Adding to function < Caesar-algorithm > to algorithms.py

--- Algorithms.py
+++ Algorithms.py
@@ -1,5 +1,6 @@
         #-------------------------------------keyless---------------------------
 from math import ceil
+import unicodedata
 def keyless(plaintext,key):
               
                 row=0
@@ -47,3 +48,34 @@ def railway_de(text,key):
                             reslute+=row[i]
             
             return ''.join(reslute)
+
+     #----------------------------------------------------------------
+def Caesar_algorithm(Plaint, KEY):
+
+            
+            Arabic_encryption = ""
+            English_encryption = ""
+
+            for s in range(97, 123):
+                English_encryption += chr(s)
+            for v in range(1568, 1611):
+                Arabic_encryption += chr(v)
+
+            result = ""
+            result1 = ""
+
+            for i in Plaint:
+                cat = unicodedata.name(i).split()[0]#
+                match cat:
+                    case "LATIN":
+                        n_encryption = (English_encryption.index(i.lower()) + KEY) % len(English_encryption)
+                        if i.isupper():
+                            result += English_encryption[n_encryption].upper()
+                        else:
+                            result += English_encryption[n_encryption]
+                    case "ARABIC":
+                        n_encryption1 = (Arabic_encryption.index(i) + KEY) % len(Arabic_encryption)
+                        result1 += Arabic_encryption[n_encryption1]
+
+            return result1 + result
+        
