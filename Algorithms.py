9eadb7c8a14cd90fb9497a5e277ab9c1a5e4bf0c
Abdulkareem <abdulkaree4m@gma1l.com>
2025-09-26 16:46:02 +0300

    Update  Algorithms  Multiplicative

--- Algorithms.py
+++ Algorithms.py
@@ -67,3 +67,14 @@ def Caesar_algorithm_De(text, key):
     return Caesar_algorithm(text, -key)
 
 
+# ---------------- Multiplicative ---------------- #
+def Multiplicative(text, key):
+    result = ""
+    for char in text:
+        if char.isalpha():
+            shift = 65 if char.isupper() else 97
+            result += chr(((ord(char) - shift) * key) % 26 + shift)
+        else:
+            result += char
+    return result
+
