de81f9338d6bd6e1fc1031269a140446f4925dfc
Abdulkareem <abdulkaree4m@gma1l.com>
2025-09-26 16:50:11 +0300

    Update  Algorithms RC4 function PRGA

--- Algorithms.py
+++ Algorithms.py
@@ -89,3 +89,11 @@ def KSA(key):
         j = (j + S[i] + key[i % key_length]) % 256
         S[i], S[j] = S[j], S[i]
     return S
+def PRGA(S):
+    i = j = 0
+    while True:
+        i = (i + 1) % 256
+        j = (j + S[i]) % 256
+        S[i], S[j] = S[j], S[i]
+        K = S[(S[i] + S[j]) % 256]
+        yield K
