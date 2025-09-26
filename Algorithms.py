abd1d87faacd69aa089b6a9a38e468f807822eae
Abdulkareem <abdulkaree4m@gma1l.com>
2025-09-26 16:43:43 +0300

    Update  Algorithms  Keyless

--- Algorithms.py
+++ Algorithms.py
@@ -1,55 +1,58 @@
-#-------------------------------------keyless---------------------------
-from math import ceil
-import unicodedata
-def keyless(plaintext,key):
-              
-                row=0
-                d=False  
-                
-                l=[''] *key   
-                                     #* 4
-                for i in plaintext:
-
-                    l[row]+=i
-                    if row==0 or row== key-1: # 0    -1
-                        d=not d    # 1 
-                        
-                        if d:
-                                    row+=1
-                        else:
-                                    row-=1 
-
-                cip=''.join(l)
-
-                return cip 
+#---
 #//////////////////////////////////////////////////////
-def railway_de(text,key):
-        
-            c=0
-            reslute=""
-            col=len(text)/key
-
-            if type(col)is float:
-                    col=ceil(col)
-
-            arr=[]
-            for i in range(key):
-                    row=[]
-                    for j in range(col):
-                            row.append("")
-                    arr.append(row)
-            for i in range(key):
-                    for j in range(col):
-                            if c<len(text):
-                                    arr[i][j]=text[c]
-                                    c+=1
-            for i in range(len(max(arr,key=len))):
-                    for row in arr:
-                            reslute+=row[i]
-            
-            return ''.join(reslute)
-
      #----------------------------------------------------------------
+
+# ---------------- Keyless / Railway ---------------- #
+def keyless(text, key):
+    return railway(text, key)
+
+def railway(text, key):
+    rail = [['\n' for i in range(len(text))] for j in range(key)]
+    dir_down = False
+    row, col = 0, 0
+    for i in range(len(text)):
+        if row == 0 or row == key - 1:
+            dir_down = not dir_down
+        rail[row][col] = text[i]
+        col += 1
+        row += 1 if dir_down else -1
+    result = []
+    for i in range(key):
+        for j in range(len(text)):
+            if rail[i][j] != '\n':
+                result.append(rail[i][j])
+    return "".join(result)
+
+def railway_de(cipher, key):
+    rail = [['\n' for i in range(len(cipher))] for j in range(key)]
+    dir_down = None
+    row, col = 0, 0
+    for i in range(len(cipher)):
+        if row == 0:
+            dir_down = True
+        if row == key - 1:
+            dir_down = False
+        rail[row][col] = '*'
+        col += 1
+        row += 1 if dir_down else -1
+    index = 0
+    for i in range(key):
+        for j in range(len(cipher)):
+            if rail[i][j] == '*' and index < len(cipher):
+                rail[i][j] = cipher[index]
+                index += 1
+    result = []
+    row, col = 0, 0
+    for i in range(len(cipher)):
+        if row == 0:
+            dir_down = True
+        if row == key - 1:
+            dir_down = False
+        if rail[row][col] != '*':
+            result.append(rail[row][col])
+            col += 1
+        row += 1 if dir_down else -1
+    return "".join(result)
 
 # ---------------- Caesar (Additive) ---------------- #
 def Caesar_algorithm(text, key):
